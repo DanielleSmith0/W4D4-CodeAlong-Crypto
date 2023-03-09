a7a5a998bdbbb95b70bb19c1b772c33441f7d23c
DanielleSmith0 <daniellesmith@Danielles-MacBook-Pro.local>
2023-03-09 14:48:55 -0700

    initial commit/needToFixErrors

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors())
+
+//endpoints
+
+const {createMessage} = require("./controller")
+
+app.post("/api/messages", createMessage);
+
+app.listen(4004, () => console.log("server running on 4004"));
